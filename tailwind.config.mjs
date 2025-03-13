9721dde20a0180c8e79a64f0bebdc8dee8e001e2
Yuliana Mallorga <yulianamallorga@gmail.com>
2025-03-13 20:35:09 -0300

    Set up dark mode.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
